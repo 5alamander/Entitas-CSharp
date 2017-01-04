4f350a3d9a744900eab60cb035c4a76e23a7ab8b
Simon Schmid <mail@sschmid.com>
2017-01-04 14:59:08 +0100

    New folder structure

---
+++ Entitas/Entitas/Plugins/Extensions/CopyEntityExtension.cs
@@ -0,0 +1,27 @@
+﻿namespace Entitas {
+
+    public static class CopyEntityExtension {
+
+        public static void CopyTo<TEntity>(this TEntity entity, TEntity target, bool replaceExisting = false, params int[] indices)
+            where TEntity : class, IEntity, new() {
+            var componentIndices = indices.Length == 0
+                                          ? entity.GetComponentIndices()
+                                          : indices;
+
+            for(int i = 0; i < componentIndices.Length; i++) {
+                var index = componentIndices[i];
+                var component = entity.GetComponent(index);
+                var clonedComponent = target.CreateComponent(
+                    index, component.GetType()
+                );
+                component.CopyPublicMemberValues(clonedComponent);
+
+                if(replaceExisting) {
+                    target.ReplaceComponent(index, clonedComponent);
+                } else {
+                    target.AddComponent(index, clonedComponent);
+                }
+            }
+        }
+    }
+}
--- Entitas/Entitas/Plugins/Serialization/Blueprints/ComponentBlueprint.cs
+++ Entitas/Entitas/Plugins/Serialization/Blueprints/ComponentBlueprint.cs
@@ -86,10 +86,4 @@ namespace Entitas.Serialization.Blueprints {
             return component;
         }
     }
-
-    public class ComponentBlueprintException : EntitasException {
-        public ComponentBlueprintException(string message, string hint) :
-            base(message, hint) {
-        }
-    }
 }
